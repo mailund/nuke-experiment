1438479edcf97176d7abdc56338c63a9f8e9b751
Thomas Mailund <tm@kvantify.dk>
2025-06-26 10:48:16 +0200

    Loading factories from dynlibs

--- NativeInterface/NativeAgentFactory.cs
+++ NativeInterface/NativeAgentFactory.cs
@@ -21,4 +21,19 @@ public class NativeAgentFactory
         var vtable = Marshal.PtrToStructure<AgentVtable>(vtablePtr);
         return new CNativeAgent(agentHandle, vtable);
     }
+
+    public static NativeAgentFactory LoadFromLibrary(string libraryPath)
+    {
+        // Load the native library
+        IntPtr libHandle = NativeLibrary.Load(libraryPath);
+
+        // Get the address of the agent_factory symbol
+        if (!NativeLibrary.TryGetExport(libHandle, "agent_factory", out IntPtr symbolPtr))
+            throw new InvalidOperationException("Could not find 'agent_factory' symbol in library.");
+
+        // Marshal the symbol as a delegate
+        var factory = Marshal.GetDelegateForFunctionPointer<AgentFactory>(symbolPtr);
+
+        return new NativeAgentFactory(factory);
+    }
 }
